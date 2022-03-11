008cd2a53dc3fda68d16546de4c2254a0a1a8b86
LightQuantum <self@lightquantum.me>
2022-03-11 08:42:47 +0800

    feat: support teloxide 0.7.x

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use teloxide::dispatching::update_listeners;
 use teloxide::dispatching::update_listeners::UpdateListener;
-use teloxide::prelude::*;
+use teloxide::requests::Requester;
 use teloxide::RequestError;
 
 #[cfg(feature = "either")]
@@ -53,8 +53,8 @@ impl Listener {
         bot: R,
     ) -> Either<impl UpdateListener<R::Err>, impl UpdateListener<R::Err>>
     where
-        R: Requester<Err = RequestError> + 'static,
-        <R as Requester>::GetUpdatesFaultTolerant: Send,
+        R: Requester<Err = RequestError> + Send + 'static,
+        <R as Requester>::GetUpdates: Send,
     {
         match self {
             Listener::Polling => Either::Left(update_listeners::polling_default(bot).await),
@@ -66,8 +66,8 @@ impl Listener {
     #[allow(clippy::future_not_send)]
     pub async fn build<R>(self, bot: R) -> impl UpdateListener<R::Err>
     where
-        R: Requester<Err = RequestError> + 'static,
-        <R as Requester>::GetUpdatesFaultTolerant: Send,
+        R: Requester<Err = RequestError> + Send + 'static,
+        <R as Requester>::GetUpdates: Send,
     {
         match self {
             Listener::Polling => update_listeners::polling_default(bot).await,
